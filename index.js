be28c1d402645a36e4d03f43d54cd1d96e7706ef
Whsxg <shabanov.matveyka@bk.ru>
2026-09-24 10:19:06 +0300

    Update index.js

--- index.js
+++ index.js
@@ -24,7 +24,7 @@ const { startWithdrawConfirmCron } = require('./cron/withdrawConfirmCron');
 const app = express();
 
 app.use(cors({
-  origin: 'https://hate-upgrade.vercel.app',
+  origin: 'https://hate-caps-v2.vercel.app',
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization'],
   credentials: true
